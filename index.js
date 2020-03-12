988803dfbaaf2eab3e187925108805ea1267c946
lassebramer <lassebramer@gmail.com>
2020-03-12 23:40:30 +0100

    text?

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var button = document.getElementById("getTree");
 button.onclick = sendData;
 
 async function sendData() {
-  data = await (await fetch("/.netlify/functions/api")).body;
+  data = await (await fetch("/.netlify/functions/api")).text();
   console.log(data);
 }
